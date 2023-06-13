54b8e014e181d0f4e5358cf7dec9b323887a221b
rohitashgoswami <rohitash0510@gmail.com>
2023-06-13 12:21:17 +0530

    added console consept

---
+++ script.js
@@ -0,0 +1,47 @@
+/* What is JavaScript?
+    JavaScript is.....
+1. High level, interpreted programming language
+2. Vonnforms to the ECMAScritp specification
+3. Multi-Paradigm 
+4. Runs on the client/browser as well as on the server (Node.js) 
+
+
+Why learn JavaScript ??
+
+1. It is the programming language of the browser
+2. Build very interactive user interfaces with frameworks like React
+3. Used in building very fast server side and full stack applications
+4. Used in mobile development (React Native, NativeScript, lonic)
+Used in desktop application development (Electron JS)
+
+Topics is this tutorial.....
+
+1. Variables & Data Types
+2. Arrays 
+3. Object Literals
+4. Methods for strings, arrays, objects, etc
+5. Loops - for, while, for...of, forEach, map
+6. Conditionals(if, ternary & switch)
+7. Functionns(normal & arrow)
+8. OOP (prototypes & classes)
+9. DOM Selection
+10. DOM manipulation
+11. Events
+12. Basic Form Validation*/
+
+
+// let's jump in the code....
+
+// this is for directly give a massege or pop-up
+// alert('Hello world');
+
+// this is for print the value..
+ /*console.log('Hello world');
+ console.error('This is an error');
+ console.warn('This is a warning'); */
+ 
+//  we can learn about console on this website => https://developer.mozilla.org/en-US/docs/Web/API/console  
+
+ Let's talk about Variables
+
+
